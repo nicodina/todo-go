d8aefcdd01375c943a1afca5b3d0cb4495cb1bc3
ndinardo <nicola.dinardo@ticketeasy.it>
2020-12-22 19:31:55 +0100

    writing a first handler

--- main.go
+++ main.go
@@ -5,8 +5,18 @@ import (
 	"log"
 )
 func main() {
-
 	const PORT = "8080"
+	log.Print("Running on port " + PORT)
+
+	// Handlers
+	http.HandleFunc("/", HomeTodo)
+
+	// Starting the server 
 	log.Fatal(http.ListenAndServe(":"+PORT, nil))
 
+}
+
+// HomeTodo writes back the path to the user
+func HomeTodo(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(r.URL.Path))
 }
