c386a60fbce9b22bb8513f65d60c578397348b3e
ndinardo <nicola.dinardo@ticketeasy.it>
2020-12-25 11:50:30 +0100

    Adding upload file endpoint + client

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strings"
+	"os"
 	"log"
 	"net/http"
 	"html/template"
@@ -35,13 +36,8 @@ func login(w http.ResponseWriter, r *http.Request) {
 	log.Println("Login ", r.Method, " request ...")
 
 	if r.Method == "GET" {
-		crutime := time.Now().Unix()
-		h := md5.New()
-		io.WriteString(h, strconv.FormatInt(crutime, 10))
-		token := fmt.Sprintf("%x", h.Sum(nil))
-
 		t, _ := template.ParseFiles("login.html")
-		t.Execute(w, token)
+		t.Execute(w, createToken())
 	} else {
 		r.ParseForm()
 
@@ -56,12 +52,51 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+
+	log.Println("Upload ", r.Method, " request ...")
+
+   	if r.Method == "GET" {
+       	t, _ := template.ParseFiles("upload.html")
+       	t.Execute(w, createToken())
+   	} else {
+       	r.ParseMultipartForm(32 << 20)
+       	file, handler, err := r.FormFile("uploadfile")
+       	if err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+           	return
+       	}
+       	defer file.Close()
+       	fmt.Fprintf(w, "%v", handler.Header)
+       	f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+       	if err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+           	return
+       	}
+       	defer f.Close()
+       	io.Copy(f, file)
+   	}
+}
+
+func createToken() string {
+	
+	crutime := time.Now().Unix()
+	h := md5.New()
+
+	io.WriteString(h, strconv.FormatInt(crutime, 10))
+
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
 func main() {
 
 	log.Print("Running on port " + port)
 
 	// Handlers
 	http.HandleFunc("/login", login)
+	http.HandleFunc("/upload", uploadFile)
 	http.HandleFunc("/", sayhelloName)
 
 	http.Handle("/static/", http.FileServer(http.Dir("public")))
